ad08aef39c79229561d4088746dab04bcc6548b3
Agastyasourcecodes <139317735+Agastyasourcecodes@users.noreply.github.com>
2023-08-08 12:59:18 +0530

    Add files via upload

---
+++ 18wirh_arguments_without_returnvalue.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void product(int x, int y);
+
+int main(){
+product(5,3);
+return 0;}
+
+void product(int a , int b)
+
+{int multiplication;
+
+multiplication=a*b;
+printf("the product is%d\n",multiplication);}
