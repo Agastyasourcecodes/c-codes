a619533040d875d401036103bf671da2d6038123
Agastyasourcecodes <139317735+Agastyasourcecodes@users.noreply.github.com>
2023-08-09 11:59:05 +0530

    Add files via upload

---
+++ 21Recursive_functions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int factorial(int number)
+{ if(number==1 || number==0)
+{return 1;}
+else
+ {  
+    return number*factorial(number-1);//recursion of function
+ }    }
+
+ int main()
+ {int num;
+ printf("enter a no.:");
+ scanf("%d",&num);
+ printf("\nthefactorial of %d is %d",num,factorial(num));
+ return 0;
+ 
+}
